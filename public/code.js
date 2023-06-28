5d7e5915a9bbc7581dc246732f5f1eb861d8e50b
Lukwago-LR <lukwagorsaved@gmail.com>
2023-06-28 14:48:02 +0200

    new version

--- public/code.js
+++ public/code.js
@@ -6,96 +6,104 @@ function initFunc() {
         medium: 0,
         large: 0,
         total: 0,
-        varVal:0,
+        varVal: 0,
         closePay: true,
         isShowing: false,
         finalMessage: "",
         showMessage: false,
         userPrice: 0,
-        apiInfo:{},
-        addFirst:'',
-        async init(){
-            this.apiInfo = await (await fetch('https:pizza-api.projectcodex.net/api/pizzas')).json()
-            console.log(this.apiInfo)
+        apiInfo: {},
+        addFirst: '',
+         init() {
+         
+            this.apiInfo = axios.get('https:pizza-api.projectcodex.net/api/pizzas')
+                .then(function (response) {
+                    return response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    return {};
+                })
         },
 
         increaseSmall: function () {
-            if(this.varVal >0){
+            if (this.varVal > 0) {
                 this.small += this.varVal;
                 this.total = this.small + this.medium + this.large;
-            }else{
-                this.addFirst="Choose pizza before additions or subtractions";
-                setTimeout(()=>{
-                    this.addFirst='';                                       
-                },3000);             
+            } else {
+                this.addFirst = "Choose pizza before additions or subtractions";
+                setTimeout(() => {
+                    this.addFirst = '';
+                }, 3000);
 
             }
         },
 
         increaseMedium: function () {
-            if(this.varVal> 0){
+            if (this.varVal > 0) {
                 this.medium += this.varVal;
                 this.total = this.small + this.medium + this.large;
-            } else{
-                this.addFirst="Choose pizza before additions or subtractions";
-                setTimeout(()=>{
-                    this.addFirst='';                                       
-                },3000);              }         
+            } else {
+                this.addFirst = "Choose pizza before additions or subtractions";
+                setTimeout(() => {
+                    this.addFirst = '';
+                }, 3000);
+            }
         },
 
         increaseLarge: function () {
-            if(this.varVal> 0){
+            if (this.varVal > 0) {
                 this.total = this.small + this.medium + this.large;
                 this.large += this.varVal;
-            }else{
-                this.addFirst="Choose pizza before additions or subtractions";
-                setTimeout(()=>{
-                    this.addFirst='';                                       
-                },3000);                
-            }              
+            } else {
+                this.addFirst = "Choose pizza before additions or subtractions";
+                setTimeout(() => {
+                    this.addFirst = '';
+                }, 3000);
+            }
         },
 
         decreaseSmall: function () {
-            if(this.varVal>0){
+            if (this.varVal > 0) {
                 if (this.small > 0) {
                     this.small -= this.varVal;
                     this.total = this.small + this.medium + this.large;
                 }
-            }else{
-                this.addFirst="Choose pizza before additions or subtractions";
-                setTimeout(()=>{
-                    this.addFirst='';                                       
-                },3000);              
-            }           
+            } else {
+                this.addFirst = "Choose pizza before additions or subtractions";
+                setTimeout(() => {
+                    this.addFirst = '';
+                }, 3000);
+            }
         },
 
         decreaseMedium: function () {
-            if(this.varVal>0){
+            if (this.varVal > 0) {
                 if (this.medium > 0) {
                     this.medium -= this.varVal;
                     this.total = this.small + this.medium + this.large;
                 }
-            }else{
-                this.addFirst="Choose pizza before additions or subtractions";
-                setTimeout(()=>{
-                    this.addFirst='';                                       
-                },3000);  
-            }            
+            } else {
+                this.addFirst = "Choose pizza before additions or subtractions";
+                setTimeout(() => {
+                    this.addFirst = '';
+                }, 3000);
+            }
         },
 
         decreaseLarge: function () {
-            if(this.varVal>0){
+            if (this.varVal > 0) {
                 if (this.large > 0) {
                     this.large -= this.varVal;
                     this.total = this.small + this.medium + this.large;
                 }
             }
-            else{
-                this.addFirst="Choose pizza before additions or subtractions";
-                setTimeout(()=>{
-                    this.addFirst='';                                       
-                },3000);               
-            }             
+            else {
+                this.addFirst = "Choose pizza before additions or subtractions";
+                setTimeout(() => {
+                    this.addFirst = '';
+                }, 3000);
+            }
         },
 
         checkOut: function () {
